22e218dcdfa3d4a7fb2eaf98007b56b431415ddb
mestrevh <victor-torugo@hotmail.com>
2026-01-22 10:19:28 -0300

    [code-refactoring]: init

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+"""
+Projeto criado para estudos de casos no TCC
+Aluno: Victor Hugo Silva Ângelo
+Orientador: Evandro de Barros Costa
+"""
+
+__version__ = "0.1.0"
+__auth__ = "Victor Hugo Silva Angelo"
