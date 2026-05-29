45a722c2264984e955aebd600b450e94aa368a7d
mestrevh <victor-torugo@hotmail.com>
2026-05-29 15:02:31 -0300

    feat: added the buttons with files json to response llm

--- frontend/js/graphics.js
+++ frontend/js/graphics.js
@@ -31,8 +31,6 @@ function graphicBuild(urlData, title) {
       const yArrayTheHuxley = Object.values(statusTheHuxley);
       const yArrayLLM = xArray.map((key) => statusLLMJudger[key] || 0);
 
-      console.log(yArrayLLM);
-
       const dataGraphics = [
         {
           x: xArray,
@@ -70,7 +68,39 @@ function graphicBuild(urlData, title) {
     });
 }
 
+const gpt = document.getElementById("btn-gpt5-4-nano");
+const gemini = document.getElementById("btn-gemini-3-flash");
+
+gpt.addEventListener("click", () => {
+  if (
+    gpt.classList.contains("bg-gray-200") &&
+    gpt.classList.contains("text-gray-700")
+  ) {
+    gpt.classList.remove("bg-gray-200", "text-gray-700");
+    gpt.classList.add("bg-blue-600", "text-white");
+    gemini.classList.remove("bg-blue-600", "text-white");
+    gemini.classList.add("bg-gray-200", "text-gray-700");
+    console.log("Sou gpt");
+  }
+});
+
+gemini.addEventListener("click", () => {
+  if (
+    gemini.classList.contains("bg-gray-200") &&
+    gemini.classList.contains("text-gray-700")
+  ) {
+    gemini.classList.remove("bg-gray-200", "text-gray-700");
+    gemini.classList.add("bg-blue-600", "text-white");
+    gpt.classList.remove("bg-blue-600", "text-white");
+    gpt.classList.add("bg-gray-200", "text-gray-700");
+    console.log("Sou gemini");
+  }
+});
+
 const urlData =
   "/create-code-and-judge-code-with-llms/database/output/json/simulation_the_huxley_gpt_5_4_nano.json";
 
-graphicBuild(urlData, "Gemini 3 Flash Preview no The Huxley e o GPT 5.4 Nano simulando o The Huxley");
+graphicBuild(
+  urlData,
+  "Gemini 3 Flash Preview no The Huxley e o GPT 5.4 Nano simulando o The Huxley",
+);
